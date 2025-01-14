87e3bea020f14a8b5aa45366975829d5b60a1adf
sean-one <basement365@gmail.com>
2025-01-13 16:01:59 -0800

    more testing work and some styling too

--- javascript/main.js
+++ javascript/main.js
@@ -1,105 +1,121 @@
+
 document.addEventListener('DOMContentLoaded', function() {
     adjustFontSize()
-    displayScreenSize()
-    startScreenSaver();
+    // serviceDetailRotation()
+    // startScreenSaver();
 });
 
 document.addEventListener('resize', function() {
     adjustFontSize()
-    displayScreenSize()
 });
 
-function startScreenSaver() {
-    const menuDisplayTime = 15000; // 15 seconds for TESTING
-    const slideshowImageInterval = 5000; // 5 seconds for TESTING
-    const slideshowElement = document.getElementById('slideshow');
-    const menuElement = document.querySelector('.menu-wrapper');
+const test_data_path = '../utils/before-after-test-data.json';
 
-   // Dynamically define your image sets here
-    const imageSets = [
-        {
-            before: '../images/before-n-after/before-after-set-01.webp',
-            after: '../images/before-n-after/before-after-set-02.webp' 
-        },
-        {
-            before: '../images/before-n-after/before-after-set-03.webp',
-            after: '../images/before-n-after/before-after-set-04.webp'
-        },
-        {
-            before: '../images/before-n-after/before-after-set-05.webp',
-            after: '../images/before-n-after/before-after-set-06.webp'
-        },
-        {
-            before: '../images/before-n-after/before-after-set-07.webp',
-            after: '../images/before-n-after/before-after-set-08.webp'
-        },
-        // Add more pairs as needed
-    ];
-
-    let currentSetIndex = 0;
-
-    function showMenu() {
-        menuElement.style.display = 'flex';
-        slideshowElement.style.display = 'none';
-
-        // Return to the slideshow after the menu display time
-        setTimeout(showSlideshow, menuDisplayTime);
-    }
-
-    function showSlideshow() {
-        menuElement.style.display = 'none';
-        slideshowElement.style.display = 'flex';
-
-        function displayImagePair() {
-            // Clear and update slideshow content dynamically
-            slideshowElement.innerHTML = ''; // Clear previous content
-
-            if (currentSetIndex >= imageSets.length) {
-                currentSetIndex = 0; // Reset to the first set
-            }
-
-            const beforeImage = document.createElement('img');
-            beforeImage.src = imageSets[currentSetIndex].before;
-            beforeImage.classList.add('slideshow-image');
-
-            const afterImage = document.createElement('img');
-            afterImage.src = imageSets[currentSetIndex].after;
-            afterImage.classList.add('slideshow-image');
-
-            slideshowElement.appendChild(beforeImage);
-            slideshowElement.appendChild(afterImage);
-
-            // Show "before" image, then "after" image with a delay
-            beforeImage.style.display = 'block';
-            setTimeout(() => {
-                beforeImage.style.display = 'none';
-                afterImage.style.display = 'block';
-            }, slideshowImageInterval / 2);
-
-            currentSetIndex++;
+fetch(test_data_path)
+    .then(resp => {
+        if (!resp.ok) {
+            throw new Error(`http error! status: ${resp.status}`);
         }
+        return resp.json();
+    })
+    .then(data => {
+        console.log(data)
+    })
+    .catch(error => {
+        console.error(`error fetching json:`, error);
+    });
 
-        // Immediately display the first pair
-        displayImagePair()
+// function startScreenSaver() {
+//     const menuDisplayTime = 15000; // 15 seconds for TESTING
+//     const slideshowImageInterval = 5000; // 5 seconds for TESTING
+//     const slideshowElement = document.getElementById('slideshow');
+//     const menuElement = document.querySelector('.menu-wrapper');
 
-        // Start cycling through image pairs
-        const slideshowCycle = setInterval(displayImagePair, slideshowImageInterval);
+//    // Dynamically define your image sets here
+//     const imageSets = [
+//         {
+//             before: '../images/before-n-after/before-after-set-01.webp',
+//             after: '../images/before-n-after/before-after-set-02.webp' 
+//         },
+//         {
+//             before: '../images/before-n-after/before-after-set-03.webp',
+//             after: '../images/before-n-after/before-after-set-04.webp'
+//         },
+//         {
+//             before: '../images/before-n-after/before-after-set-05.webp',
+//             after: '../images/before-n-after/before-after-set-06.webp'
+//         },
+//         {
+//             before: '../images/before-n-after/before-after-set-07.webp',
+//             after: '../images/before-n-after/before-after-set-08.webp'
+//         },
+//         // Add more pairs as needed
+//     ];
 
-        // Stop slideshow and return to menu after all image pairs are shown
-        setTimeout(() => {
-            clearInterval(slideshowCycle);
-            showMenu();
-        }, slideshowImageInterval * imageSets.length);
-    }
+//     let currentSetIndex = 0;
 
-    // Start with the menu
-    showMenu();
-}
+//     function showMenu() {
+//         menuElement.style.display = 'flex';
+//         slideshowElement.style.display = 'none';
+
+//         // Return to the slideshow after the menu display time
+//         setTimeout(showSlideshow, menuDisplayTime);
+//     }
+
+//     function showSlideshow() {
+//         menuElement.style.display = 'none';
+//         slideshowElement.style.display = 'flex';
+
+//         function displayImagePair() {
+//             // Clear and update slideshow content dynamically
+//             slideshowElement.innerHTML = ''; // Clear previous content
+
+//             if (currentSetIndex >= imageSets.length) {
+//                 currentSetIndex = 0; // Reset to the first set
+//             }
+
+//             const beforeImage = document.createElement('img');
+//             beforeImage.src = imageSets[currentSetIndex].before;
+//             beforeImage.classList.add('slideshow-image');
+
+//             const afterImage = document.createElement('img');
+//             afterImage.src = imageSets[currentSetIndex].after;
+//             afterImage.classList.add('slideshow-image');
+
+//             slideshowElement.appendChild(beforeImage);
+//             slideshowElement.appendChild(afterImage);
+
+//             // Show "before" image, then "after" image with a delay
+//             beforeImage.style.display = 'block';
+//             setTimeout(() => {
+//                 beforeImage.style.display = 'none';
+//                 afterImage.style.display = 'block';
+//             }, slideshowImageInterval / 2);
+
+//             currentSetIndex++;
+//         }
+
+//         // Immediately display the first pair
+//         displayImagePair()
+
+//         // Start cycling through image pairs
+//         const slideshowCycle = setInterval(displayImagePair, slideshowImageInterval);
+
+//         // Stop slideshow and return to menu after all image pairs are shown
+//         setTimeout(() => {
+//             clearInterval(slideshowCycle);
+//             showMenu();
+//         }, slideshowImageInterval * imageSets.length);
+//     }
+
+//     // Start with the menu
+//     showMenu();
+// }
 
 function adjustFontSize() {
     const screenWidth = window.innerWidth;
     const screenHeight = window.innerHeight;
-    const pixelRatio = window.devicePixelRatio || 1;
+    // const pixelRatio = window.devicePixelRatio || 1;
     const orientation = (screenWidth > screenHeight) ? 'landscape' : 'portrait';
     
     let baseFontSizePercentage;
@@ -116,14 +132,8 @@ function adjustFontSize() {
     console.log(document.documentElement.style.fontSize)
 }
 
-function displayScreenSize() {
-    const screenWidth = window.innerWidth;
-    const screenHeight = window.innerHeight;
-    const pixelRatio = window.devicePixelRatio || 1;
-    const screenSizeElement = document.getElementById('screen-details');
-    const orientation = (screenWidth > screenHeight) ? 'landscape' : 'portrait';
 
-    if (screenSizeElement) {
-        screenSizeElement.textContent = `width: ${screenWidth}, height: ${screenHeight}, ratio: ${pixelRatio}, orientation: ${orientation}`;
-    }
+
+function serviceDetailRotation() {
+    console.log(test_data)
 }
